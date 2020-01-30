e6b42b2471cc67d83458c04ce8f53f28e1c6f32f
Oskari Heinonen <oskariheinonen@Oskaris-MBP.home>
2020-01-30 13:30:12 +0200

    test

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('login/', views.signup, name='login'),
+    path('logout/', views.signup, name='logout'),
 ]
