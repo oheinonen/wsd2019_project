3fc2a5f4d0301bd63c8178d9642385a0561fb66b
Oskari Heinonen <oskariheinonen@Oskaris-MBP.home>
2020-02-04 17:21:51 +0200

    Add&Delete games

--- gamestore/urls.py
+++ gamestore/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from .views import (
+    GameDetailView,
+    GameListView,
+    GameCreateView,
+    GameDeleteView
+        )
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('game/add/',GameCreateView.as_view(),name='game-add' ),
+    path('game/<str:pk>/',GameDetailView.as_view(),name='game-detail' ),
+    #path('game/<str:pk>/update/',GameUpdateView.as_view(),name='game-update'),
+    path('game/<str:pk>/delete/',GameDeleteView.as_view(),name='game-delete')
+
 ]
--- gamestore/views.py
+++ gamestore/views.py
@@ -26,3 +26,25 @@ class GameListView(ListView):
 
 class GameDetailView(DetailView):
     model = Game
+
+
+class GameCreateView(LoginRequiredMixin,UserPassesTestMixin, CreateView):
+    model = Game
+    fields = ['name', 'url', 'price']
+
+    def form_valid(self,form):
+        form.instance.developer = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        return self.request.user.usertype == "['dev']"
+
+
+
+class GameDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Game
+    success_url = '/'
+
+    def test_func(self):
+        game = self.get_object()
+        return self.request.user == game.developer
