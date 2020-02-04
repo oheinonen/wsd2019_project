b12ae8b4865eeb72b677c02ec94b987d96dc1b5a
Oskari Heinonen <oskariheinonen@Oskaris-MBP.home>
2020-02-04 17:19:50 +0200

    login/logout, game-detail

--- gamestore/views.py
+++ gamestore/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from .models import Game
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 import requests
-
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html")
+    context = {
+        'games' : Game.objects.all()
+    }
+    return render(request, "home.html",context)
+
+class GameListView(ListView):
+    model = Game
+    template_name = 'home.html'
+    context_object_name = 'games'
+
+class GameDetailView(DetailView):
+    model = Game
