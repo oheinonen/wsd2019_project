186cb9cca759533818d31e347ae549eee03ec688
JussiHuot <jussi.huotari@aalto.fi>
2020-02-13 20:15:13 +0200

    test

--- users/views.py
+++ users/views.py
@@ -45,7 +45,6 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        login(request, user)
         # return redirect('home')
         return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
     else:
